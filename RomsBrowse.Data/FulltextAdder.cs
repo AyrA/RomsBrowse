7cd351536b2c380d7d66b4a10d7caebf7570da1d
Kevin Gut <kevin.gut@bitm.sg>
2024-06-16 19:53:16 +0200

    Put fulltext index into own file

---
+++ RomsBrowse.Data/FulltextAdder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RomsBrowse.Data
+{
+    internal static class FulltextAdder
+    {
+        public static void AddFulltext(MigrationBuilder builder)
+        {
+            builder.Sql("CREATE FULLTEXT CATALOG RomsFullText AS DEFAULT", true);
+            builder.Sql("CREATE FULLTEXT INDEX ON RomFiles(DisplayName) KEY INDEX PK_RomFiles", true);
+        }
+
+        public static void RemoveFulltext(MigrationBuilder builder)
+        {
+            builder.Sql("DROP FULLTEXT INDEX ON RomFiles", true);
+            builder.Sql("DROP FULLTEXT CATALOG RomsFullText", true);
+        }
+    }
+}
