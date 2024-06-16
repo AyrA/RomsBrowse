0a63e419c175fab9bca534341ed9d58238cf5f8a
Kevin Gut <kevin.gut@bitm.sg>
2024-06-16 19:15:56 +0200

    Increase field size

--- RomsBrowse.Data/Migrations/20240616155545_IncreaseFileNameLength.Designer.cs
+++ RomsBrowse.Data/Migrations/20240616155545_IncreaseFileNameLength.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RomsBrowse.Data;
 
@@ -10,9 +11,11 @@ using RomsBrowse.Data;
 namespace RomsBrowse.Data.Migrations
 {
     [DbContext(typeof(RomsContext))]
-    partial class RomsContextModelSnapshot : ModelSnapshot
+    [Migration("20240616155545_IncreaseFileNameLength")]
+    partial class IncreaseFileNameLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RomsBrowse.Data/Migrations/20240616155545_IncreaseFileNameLength.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RomsBrowse.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseFileNameLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Sha256",
+                table: "RomFiles",
+                type: "varbinary(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "RomFiles",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "RomFiles",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "RomFiles");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Sha256",
+                table: "RomFiles",
+                type: "varbinary(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "RomFiles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
