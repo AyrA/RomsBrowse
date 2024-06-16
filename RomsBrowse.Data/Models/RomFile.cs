e1b839d0cc69dbed987e5eb811e03d33166821d6
Kevin Gut <kevin.gut@bitm.sg>
2024-06-16 17:55:23 +0200

    Implement validation

--- RomsBrowse.Data/Models/RomFile.cs
+++ RomsBrowse.Data/Models/RomFile.cs
@@ -1,29 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RomsBrowse.Common;
 using System.ComponentModel.DataAnnotations;
 
 namespace RomsBrowse.Data.Models
 {
 #nullable disable
     [Index(nameof(FileName))]
-    public class RomFile
+    public class RomFile : IValidateable
     {
         [Key]
         public int Id { get; set; }
 
-        [Required, StringLength(100)]
+        [Required, StringLength(255)]
         public string FileName { get; set; }
+
+        [Required, StringLength(255)]
+        public string DisplayName { get; set; }
 
         [Required, StringLength(255)]
         public string FilePath { get; set; }
 
+        [Range(0, int.MaxValue)]
         public int Size { get; set; }
 
-        [MaxLength(32)]
+        [Required, MaxLength(32), MinLength(32)]
         public byte[] Sha256 { get; set; }
 
+        [Required]
         public Platform Platform { get; set; }
 
         public int PlatformId { get; set; }
+
+        public void Validate()
+        {
+            ValidationTools.ValidatePublic(this);
+        }
     }
 #nullable restore
 }
