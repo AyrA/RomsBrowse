8d9a26924575a24738ef03694446f8e949efdb2a
Kevin Gut <kevin.gut@bitm.sg>
2024-06-19 01:36:26 +0200

    Licenses

---
+++ README.md
@@ -0,0 +1,18 @@
+# RomsBrowse
+
+ROM browser with built-in emulator
+
+## Usage
+
+Edit `appsettings.json` to point to an SQL server with an empty database,
+and set the `RomDir` value to the directory that contains the roms
+
+**TODO: Instructions for the config.ini in the RomDir folder**
+
+**TODO: Make this easier to use**
+
+**TODO: Anonymous form of user accounts**
+
+**TODO: Server side save states**
+
+**TODO: Server side game memory**
--- RomsBrowse.Web/Program.cs
+++ RomsBrowse.Web/Program.cs
@@ -1,3 +1,20 @@
+/*
+    RomsBrowse - A ROM file browser with built-in emulator
+    Copyright (C) 2024  Kevin Gut
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 using AyrA.AutoDI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting.WindowsServices;
