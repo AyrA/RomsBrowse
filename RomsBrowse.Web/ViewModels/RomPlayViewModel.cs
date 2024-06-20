85fb4f41425544f129314246488a52fc13edd890
Kevin Gut <kevin.gut@bitm.sg>
2024-06-20 19:02:05 +0200

    Extend rom view model

--- RomsBrowse.Web/ViewModels/RomPlayViewModel.cs
+++ RomsBrowse.Web/ViewModels/RomPlayViewModel.cs
@@ -2,8 +2,10 @@
 
 namespace RomsBrowse.Web.ViewModels
 {
-    public class RomViewModel(RomFile rom)
+    public class RomPlayViewModel(RomFile rom)
     {
+        public bool IsDownloadingEmulator { get; set; }
+        public bool HasEmulator { get; set; }
         public string EmulatorView { get; set; } = rom.Platform.ShortName;
         public string Title { get; set; } = rom.DisplayName;
         public string FileName { get; set; } = rom.FileName;
