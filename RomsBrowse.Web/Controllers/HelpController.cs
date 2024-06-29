cdc72a7a4de7dd1909b1ec60b52154cac8d24443
Kevin Gut <kevin.gut@bitm.sg>
2024-06-29 13:50:11 +0200

    Add save game help

---
+++ RomsBrowse.Web/Controllers/HelpController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RomsBrowse.Web.Services;
+
+namespace RomsBrowse.Web.Controllers
+{
+    public class HelpController(UserService us) : BaseController(us)
+    {
+        public IActionResult Saves() => View();
+    }
+}
